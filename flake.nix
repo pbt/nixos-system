598a00ca1a616db429cac7a2d195a47e8083d93c
pb <pb@pbt.dev>
2026-08-20 21:17:16 -0400

    temporarily switch to mainline kernel

--- flake.nix
+++ flake.nix
@@ -61,7 +61,8 @@
           modules = [
             ({ pkgs, ... }: {
               nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
-              boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+              boot.kernelPackages = pkgs.linuxPackages_latest;
+              # boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
             })
             ./global/configuration.nix
             ./global/apps/firefox.nix
